753ff532a33df1ba14058e1105b9b5ecd7c203b1
Marcus Silva <mvfsillva@gmail.com>
2018-08-04 02:19:32 -0300

    Add styled-component Content

---
+++ src/styles/Content/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-bottom: 3rem;
+`
+
+export default Content

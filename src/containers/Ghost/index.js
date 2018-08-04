1076765b5c975143f3917f1d29c05fc851b84ee7
Marcus Silva <mvfsillva@gmail.com>
2018-08-04 02:50:06 -0300

    Add container example

---
+++ src/containers/Ghost/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Title from '~/components/Title'
+import Content from '~/styles/Content'
+
+const Ghost = () => (
+  <Content>
+    <Title>Duskull</Title>
+  </Content>
+)
+
+export default Ghost

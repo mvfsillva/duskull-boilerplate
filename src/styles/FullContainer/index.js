f2f563ec43bf9e180625ad15e4ae75b6d51a0459
Marcus Silva <mvfsillva@gmail.com>
2018-08-04 07:16:11 -0300

    Update color theme

--- src/styles/FullContainer/index.js
+++ src/styles/FullContainer/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const FullContainer = styled.div`
   align-items: center;
-  background-color: ${props => props.theme.colors.gray.mercury};
+  background-color: ${props => props.theme.colors.white};
   display: flex;
   height: 100vh;
   flex-direction: column;
